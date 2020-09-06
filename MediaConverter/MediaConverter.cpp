71eb6a64720e40d6389d755cc9bdccb815f78f48
dallas1287 <jdthornton1@gmail.com>
2020-09-06 17:39:55 -0400

    first commit

---
+++ MediaConverter/MediaConverter.cpp
@@ -0,0 +1,267 @@
+// MediaConverter.cpp : Defines the exported functions for the DLL.
+//
+#include "pch.h"
+#include "framework.h"
+#include "MediaConverter.h"
+
+// This is the constructor of a class that has been exported.
+CMediaConverter::CMediaConverter()
+{
+}
+
+ErrorCode CMediaConverter::loadFrame(const char* filename, int& width, int& height, unsigned char** data)
+{
+    AVFormatContext* av_format_ctx = avformat_alloc_context();
+    if (!av_format_ctx)
+        return ErrorCode::NO_FMT_CTX;
+
+    if (avformat_open_input(&av_format_ctx, filename, NULL, NULL)) //returns 0 on success
+        return ErrorCode::FMT_UNOPENED;
+
+    if (av_format_ctx->nb_streams < 1)
+        return ErrorCode::NO_STREAMS;
+
+    int vid_str_idx = -1;
+    AVCodec* av_codec = nullptr;
+    AVCodecParameters* av_codec_params = nullptr;
+
+    for (unsigned int i = 0; i < av_format_ctx->nb_streams; ++i)
+    {
+        auto stream = av_format_ctx->streams[i];
+        av_codec_params = av_format_ctx->streams[i]->codecpar;
+
+        av_codec = avcodec_find_decoder(av_codec_params->codec_id);
+        if (!av_codec)
+            return ErrorCode::NO_CODEC;
+
+        if (av_codec_params->codec_type == AVMEDIA_TYPE_VIDEO)
+        {
+            vid_str_idx = i;
+            break;
+        }
+    }
+
+    if (vid_str_idx == -1)
+        return ErrorCode::NO_VID_STREAM;
+
+    AVCodecContext* av_codec_ctx = avcodec_alloc_context3(av_codec);
+    if (!av_codec_ctx)
+        return ErrorCode::NO_CODEC_CTX;
+
+    if (avcodec_parameters_to_context(av_codec_ctx, av_codec_params) < 0)
+        return ErrorCode::CODEC_CTX_UNINIT;
+
+    if (avcodec_open2(av_codec_ctx, av_codec, NULL) < 0)
+        return ErrorCode::CODEC_UNOPENED;
+
+    AVFrame* av_frame = av_frame_alloc();
+    if (!av_frame)
+        return ErrorCode::NO_FRAME;
+
+    AVPacket* av_packet = av_packet_alloc();
+    if (!av_packet)
+        return ErrorCode::NO_PACKET;
+
+    int response;
+    while (av_read_frame(av_format_ctx, av_packet) >= 0)
+    {
+        if (av_packet->stream_index != vid_str_idx)
+            continue;
+
+        response = avcodec_send_packet(av_codec_ctx, av_packet);
+        if (response < 0)
+            return ErrorCode::PKT_NOT_DECODED;
+
+        response = avcodec_receive_frame(av_codec_ctx, av_frame);
+        if (response == AVERROR(EAGAIN) || response == AVERROR_EOF)
+            continue;
+        else if (response < 0)
+            return ErrorCode::PKT_NOT_RECEIVED;
+
+        av_packet_unref(av_packet);
+        break;
+    }
+
+    unsigned char* output = new unsigned char[static_cast<unsigned long>(av_frame->width) * static_cast<unsigned long>(av_frame->height) * 4];
+
+    SwsContext* sws_scaler_ctx = sws_getContext(av_frame->width, av_frame->height, av_codec_ctx->pix_fmt, //input
+                                                av_frame->width, av_frame->height, AV_PIX_FMT_RGB0, //output
+                                                SWS_BILINEAR, NULL, NULL, NULL); //options
+
+    if (!sws_scaler_ctx)
+        return ErrorCode::NO_SCALER;
+
+    unsigned char* dest[4] = { output, NULL, NULL, NULL };
+    int dest_linesize[4] = { av_frame->width * 4, 0, 0, 0 };
+
+    sws_scale(sws_scaler_ctx, av_frame->data, av_frame->linesize, 0, av_frame->height, dest, dest_linesize);
+    sws_freeContext(sws_scaler_ctx);
+
+    width = av_frame->width;
+    height = av_frame->height;
+    *data = output;
+
+    return ErrorCode::SUCCESS;
+}
+
+ErrorCode CMediaConverter::openVideoReader(VideoReaderState* state, const char* filename)
+{
+    auto& width = state->width;
+    auto& height = state->height;
+    auto& av_format_ctx = state->av_format_ctx;
+    auto& av_codec_ctx = state->av_codec_ctx;
+    auto& av_frame = state->av_frame;
+    auto& av_packet = state->av_packet;
+    auto& vid_str_idx = state->video_stream_index;
+    auto& sws_scaler_ctx = state->sws_scaler_ctx;
+    auto& timebase = state->timebase;
+
+    av_format_ctx = avformat_alloc_context();
+    if (!av_format_ctx)
+        return ErrorCode::NO_FMT_CTX;
+
+    if (avformat_open_input(&av_format_ctx, filename, NULL, NULL)) //returns 0 on success
+        return ErrorCode::FMT_UNOPENED;
+
+    if (av_format_ctx->nb_streams < 1)
+        return ErrorCode::NO_STREAMS;
+
+    vid_str_idx = -1;
+    AVCodec* av_codec = nullptr;
+    AVCodecParameters* av_codec_params = nullptr;
+
+    for (unsigned int i = 0; i < av_format_ctx->nb_streams; ++i)
+    {
+        auto stream = av_format_ctx->streams[i];
+        av_codec_params = av_format_ctx->streams[i]->codecpar;
+
+        av_codec = avcodec_find_decoder(av_codec_params->codec_id);
+        if (!av_codec)
+            return ErrorCode::NO_CODEC;
+
+        if (av_codec_params->codec_type == AVMEDIA_TYPE_VIDEO)
+        {
+            vid_str_idx = i;
+            width = av_codec_params->width;
+            height = av_codec_params->height;
+            timebase = av_format_ctx->streams[i]->time_base;
+            break;
+        }
+    }
+
+    if (vid_str_idx == -1)
+        return ErrorCode::NO_VID_STREAM;
+
+    state->codeName = av_codec->long_name;
+    state->start_time = av_format_ctx->streams[vid_str_idx]->start_time;
+    state->duration = av_format_ctx->streams[vid_str_idx]->duration;
+    state->avg_frame_rate = av_format_ctx->streams[vid_str_idx]->avg_frame_rate;
+
+    av_codec_ctx = avcodec_alloc_context3(av_codec);
+    if (!av_codec_ctx)
+        return ErrorCode::NO_CODEC_CTX;
+
+    if (avcodec_parameters_to_context(av_codec_ctx, av_codec_params) < 0)
+        return ErrorCode::CODEC_CTX_UNINIT;
+
+    if (avcodec_open2(av_codec_ctx, av_codec, NULL) < 0)
+        return ErrorCode::CODEC_UNOPENED;
+
+    av_frame = av_frame_alloc();
+    if (!av_frame)
+        return ErrorCode::NO_FRAME;
+
+    av_packet = av_packet_alloc();
+    if (!av_packet)
+        return ErrorCode::NO_PACKET;
+
+    return ErrorCode::SUCCESS;
+}
+
+ErrorCode CMediaConverter::readVideoReaderFrame(VideoReaderState* state, unsigned char** frameBuffer, bool requestFlush)
+{
+    auto& width = state->width;
+    auto& height = state->height;
+    auto& av_format_ctx = state->av_format_ctx;
+    auto& av_codec_ctx = state->av_codec_ctx;
+    auto& av_frame = state->av_frame;
+    auto& av_packet = state->av_packet;
+    auto& vid_str_idx = state->video_stream_index;
+    auto& sws_scaler_ctx = state->sws_scaler_ctx;
+
+    //inital frame read, gives it an initial state to branch from
+    int response = av_read_frame(av_format_ctx, av_packet);
+    if (response >= 0)
+    {
+        //send back and receive decoded frames, until frames can't be read
+        do{
+            if (av_packet->stream_index != vid_str_idx)
+                continue;
+
+            response = avcodec_send_packet(av_codec_ctx, requestFlush ? nullptr : av_packet);
+            if (response < 0)
+                return ErrorCode::PKT_NOT_DECODED;
+
+            response = avcodec_receive_frame(av_codec_ctx, av_frame);
+            if (response == AVERROR(EAGAIN) || response == AVERROR_EOF)
+                continue;
+            else if (response < 0)
+                return ErrorCode::PKT_NOT_RECEIVED;
+
+            av_packet_unref(av_packet);
+            break;
+        } while (av_read_frame(av_format_ctx, av_packet) >= 0);
+    }
+
+    if (response == AVERROR_EOF)
+    {
+        avcodec_flush_buffers(av_codec_ctx);
+        return ErrorCode::FILE_EOF;
+    }
+    //retrieve stats
+    state->frame_number = av_codec_ctx->frame_number;
+    state->pts = av_frame->pts;
+    state->pkt_dts = av_frame->pkt_dts;
+    state->key_frame = av_frame->key_frame;
+    state->pkt_size = av_frame->pkt_size;
+
+    //setup scaler
+    if (!sws_scaler_ctx)
+    {
+        sws_scaler_ctx = sws_getContext(width, height, av_codec_ctx->pix_fmt, //input
+            width, height, AV_PIX_FMT_RGB0, //output
+            SWS_BILINEAR, NULL, NULL, NULL); //options
+    }
+    if (!sws_scaler_ctx)
+        return ErrorCode::NO_SCALER;
+
+    uint64_t size = av_frame->width * av_frame->height * 4; //static_cast just gets intellisense to shut up about 4 bytes to 8 bytes conversions
+    unsigned char* output = new unsigned char[size];
+
+    unsigned char* dest[4] = {output, NULL, NULL, NULL };
+    int dest_linesize[4] = { width * 4, 0, 0, 0 };
+
+    sws_scale(sws_scaler_ctx, av_frame->data, av_frame->linesize, 0, height, dest, dest_linesize);
+
+    *frameBuffer = output;
+
+    return ErrorCode::SUCCESS;
+}
+
+ErrorCode CMediaConverter::seekToStart(VideoReaderState* state)
+{
+    if (av_seek_frame(state->av_format_ctx, state->video_stream_index, state->start_time, 0) >= 0)
+        return ErrorCode::SUCCESS;
+
+    return ErrorCode::SEEK_FAILED;
+}
+
+ErrorCode CMediaConverter::closeVideoReader(VideoReaderState* state)
+{
+    sws_freeContext(state->sws_scaler_ctx);
+    avformat_close_input(&state->av_format_ctx);
+    avformat_free_context(state->av_format_ctx);
+    av_frame_free(&state->av_frame);
+    av_packet_free(&state->av_packet);
+    return ErrorCode::SUCCESS;
+}
---
+++ MediaConverter/MediaConverter.h
@@ -0,0 +1,129 @@
+// The following ifdef block is the standard way of creating macros which make exporting
+// from a DLL simpler. All files within this DLL are compiled with the MEDIACONVERTER_EXPORTS
+// symbol defined on the command line. This symbol should not be defined on any project
+// that uses this DLL. This way any other project whose source files include this file see
+// MEDIACONVERTER_API functions as being imported from a DLL, whereas this DLL sees symbols
+// defined with this macro as being exported.
+#ifdef MEDIACONVERTER_EXPORTS
+#define MEDIACONVERTER_API __declspec(dllexport)
+#else
+#define MEDIACONVERTER_API __declspec(dllimport)
+#endif
+#include <iostream>
+
+//ffmpeg includes
+extern "C"
+{
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libswscale/swscale.h>
+}
+
+enum class ErrorCode : int
+{
+	FILE_EOF = -1,
+	SUCCESS,
+	NO_FMT_CTX,
+	FMT_UNOPENED,
+	NO_CODEC,
+	CODEC_UNOPENED,
+	NO_STREAMS,
+	NO_VID_STREAM,
+	NO_CODEC_CTX,
+	CODEC_CTX_UNINIT,
+	NO_FRAME,
+	NO_PACKET,
+	PKT_NOT_DECODED,
+	PKT_NOT_RECEIVED,
+	NO_SCALER,
+	SEEK_FAILED
+};
+
+class MEDIACONVERTER_API VideoReaderState
+{
+public:
+	VideoReaderState() {}
+	VideoReaderState(const VideoReaderState& other)
+	{
+		width = other.width;
+		height = other.height;
+		frame_buffer = other.frame_buffer;
+		av_format_ctx = other.av_format_ctx;
+		av_codec_ctx = other.av_codec_ctx;
+		av_frame = other.av_frame;
+		av_packet = other.av_packet;
+		sws_scaler_ctx = other.sws_scaler_ctx;
+		video_stream_index = other.video_stream_index;
+		timebase.num = other.timebase.num;
+		timebase.den = other.timebase.den;
+		codeName = other.codeName;
+		frame_number = other.frame_number;
+		pts = other.pts;
+		pkt_dts = other.pkt_dts;
+		key_frame = other.key_frame;
+		start_time = other.start_time;
+		duration = other.duration;
+		avg_frame_rate.num = other.avg_frame_rate.num;
+		avg_frame_rate.den = other.avg_frame_rate.den;
+		pkt_size = other.pkt_size;
+	}
+	~VideoReaderState() {}
+	bool IsEqual(const VideoReaderState& other)
+	{
+		return width == other.width &&
+			height == other.height &&
+			frame_buffer == other.frame_buffer &&
+			av_format_ctx == other.av_format_ctx &&
+			av_codec_ctx == other.av_codec_ctx &&
+			av_frame == other.av_frame &&
+			av_packet == other.av_packet &&
+			sws_scaler_ctx == other.sws_scaler_ctx &&
+			video_stream_index == other.video_stream_index &&
+			timebase.num == other.timebase.num &&
+			timebase.den == other.timebase.den &&
+			codeName == other.codeName &&
+			frame_number == other.frame_number &&
+			pts == other.pts &&
+			pkt_dts == other.pkt_dts &&
+			key_frame == other.key_frame &&
+			start_time == other.start_time &&
+			duration == other.duration &&
+			avg_frame_rate.num == other.avg_frame_rate.num &&
+			avg_frame_rate.den == other.avg_frame_rate.den &&
+			pkt_size == other.pkt_size;
+	}
+
+public:
+	int width = 0;
+	int height = 0;
+	uint8_t* frame_buffer = nullptr;
+
+	AVFormatContext* av_format_ctx = nullptr;
+	AVCodecContext* av_codec_ctx = nullptr;
+	AVFrame* av_frame = nullptr;
+	AVPacket* av_packet = nullptr;
+	SwsContext* sws_scaler_ctx = nullptr;
+	int video_stream_index = -1;
+	AVRational timebase = { -1, -1 };
+
+	std::string codeName;
+	int frame_number = -1;
+	int pts = -1;
+	int pkt_dts = -1;
+	int key_frame = -1;
+	int64_t start_time = -1;
+	int64_t duration = -1;
+	AVRational avg_frame_rate = { -1, -1 };
+	size_t pkt_size = -1;
+};
+
+// This class is exported from the dll
+class MEDIACONVERTER_API CMediaConverter {
+public:
+	CMediaConverter(void);
+	ErrorCode loadFrame(const char* filename, int& width, int& height, unsigned char** data);
+	ErrorCode openVideoReader(VideoReaderState* state, const char* filename);
+	ErrorCode readVideoReaderFrame(VideoReaderState* state, unsigned char** frame_buffer, bool requestFlush = false);
+	ErrorCode seekToStart(VideoReaderState* state);
+	ErrorCode closeVideoReader(VideoReaderState* state);
+};
